9ef8fc67a1c802eb30d56f223101a4807ec95e16
Bob Cousins <bobcousins42@googlemail.com>
2018-01-01 17:08:49 +0000

    Initial commit

---
+++ scripts/footprint-wizards/contact_wizard.py
@@ -0,0 +1,207 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+from __future__ import division
+
+import sys
+import math
+
+import pcbnew
+import HelpfulFootprintWizardPlugin as HFPW
+import PadArray as PA
+
+
+class contact_wizard(HFPW.HelpfulFootprintWizardPlugin):
+
+    def GetName(self):
+        return "Button Contact"
+
+    def GetDescription(self):
+        return "Contact for buttons"
+
+    def GenerateParameterList(self):
+
+        self.AddParam("Pads", "style", self.uNatural, 1)
+        self.AddParam("Pads", "trace width", self.uMM, 0.2)
+        self.AddParam("Pads", "trace clearance", self.uMM, 0.2)
+        self.AddParam("Pads", "diameter", self.uMM, 5)
+
+    def CheckParameters(self):
+
+        self.CheckParamInt("Pads", "*style", min_value=1, max_value=2)
+
+    def GetValue(self):
+        
+        return "contact"
+
+    def square_contact(self):
+
+        prm = self.parameters['Pads']
+        p_trace_width = prm['trace width']
+        p_trace_clearance = prm['trace clearance']
+        p_diameter = prm['diameter'];
+
+
+        spacing = p_trace_width + p_trace_clearance
+        pad_length = p_diameter - spacing 
+        radius = p_diameter/2
+        posY = -radius + p_trace_width / 2
+        alt = 0
+        
+        # draw horizontal bars
+        while posY <= radius:
+            posX = spacing * (2*alt-1) / 2
+
+            pad = PA.PadMaker(self.module).SMDPad(p_trace_width, pad_length, shape=pcbnew.PAD_SHAPE_RECT, rot_degree=0.0)
+            pos = self.draw.TransformPoint(posX, posY)
+            pad.SetPadName(1+alt)
+            pad.SetPos0(pos)
+            pad.SetPosition(pos)
+            pad.SetShape(pcbnew.PAD_SHAPE_OVAL)
+            pad.SetLayerSet(pad.ConnSMDMask())
+
+            pad.GetParent().Add(pad)
+              
+            posY = posY + spacing
+            alt = 1-alt
+
+        # vertical sides
+        
+        pad = PA.PadMaker(self.module).SMDPad(p_diameter, p_trace_width, shape=pcbnew.PAD_SHAPE_RECT, rot_degree=0.0)
+        pos = self.draw.TransformPoint(-p_diameter/2 + p_trace_width/2, 0)
+        pad.SetPadName(1)
+        pad.SetPos0(pos)
+        pad.SetPosition(pos)
+        pad.SetLayerSet(pad.ConnSMDMask())
+        pad.GetParent().Add(pad)
+      
+        pad = PA.PadMaker(self.module).SMDPad(p_diameter, p_trace_width, shape=pcbnew.PAD_SHAPE_RECT, rot_degree=0.0)
+        pos = self.draw.TransformPoint(p_diameter/2 - p_trace_width/2, 0)
+        pad.SetPadName(2)
+        pad.SetPos0(pos)
+        pad.SetPosition(pos)
+        pad.SetLayerSet(pad.ConnSMDMask())
+        pad.GetParent().Add(pad)
+
+        # 
+        body_radius = (p_diameter + self.draw.GetLineTickness())
+        self.draw.Box(0,0,body_radius, body_radius)
+
+    def round_contact(self):
+
+        prm = self.parameters['Pads']
+        p_trace_width = prm['trace width']
+        p_trace_clearance = prm['trace clearance']
+        p_diameter = prm['diameter'];
+
+        spacing = p_trace_width + p_trace_clearance
+        radius = p_diameter/2
+        circumference = (p_diameter + p_trace_width) * math.pi
+        step_angle = p_trace_width / circumference * 360
+        inner_radius = radius - p_trace_clearance - p_trace_width/2
+        # draw cross bars  
+        posY = -radius + p_trace_width
+        alt = 0
+        min_y = posY
+ 
+        while posY <= radius - p_trace_width:
+            len1 = math.sqrt (radius * radius - posY * posY)
+            #angle  = math.degrees(math.asin ((abs(posY + p_trace_width/2) ) / inner_radius))
+            #gap = p_trace_clearance / math.sin(math.radians(angle))
+            #len2 = len1 - gap
+            t = inner_radius * inner_radius - (abs(posY) + p_trace_width/2*1.2) ** 2
+            if t>0:
+                len2 = math.sqrt (t) # - abs(math.sin(math.radians(angle)) * p_trace_width )
+            else:
+                len2 = 0
+            #len2 = len1 - spacing
+            pad_length = len1 + len2  
+            posX = (len1 - pad_length/2 ) * (2*alt-1)
+            
+            pad = PA.PadMaker(self.module).SMDPad(p_trace_width, pad_length, shape=pcbnew.PAD_SHAPE_OVAL, rot_degree=0)
+            pos = self.draw.TransformPoint(posX, posY)
+            pad.SetPadName(1+alt)
+            pad.SetPos0(pos)
+            pad.SetPosition(pos)
+            pad.SetLayerSet(pad.ConnSMDMask())
+            pad.GetParent().Add(pad)
+            
+            max_y = posY 
+            posY = posY + spacing
+            alt = 1-alt
+
+        angle1 = math.degrees(math.asin (min_y/radius))
+        angle2 = math.degrees(math.asin (max_y/radius))
+        
+        # draw the outer parts as an arc composed of small pads
+        alt = 0
+        for j in [0,1]:
+            if j==0:        
+                start_angle = 180 - angle2
+                last_angle =  180 - angle1
+            else:
+                start_angle = angle1
+                last_angle = angle2
+            
+            angle = start_angle
+            while angle <= last_angle:
+                posX = math.cos (math.radians(angle)) * radius
+                posY = math.sin (math.radians(angle)) * radius
+                pad = PA.PadMaker(self.module).SMDPad(p_trace_width, p_trace_width, shape=pcbnew.PAD_SHAPE_RECT, rot_degree=-angle)
+                pos = self.draw.TransformPoint(posX, posY)
+                pad.SetPadName(1+j)
+                pad.SetPos0(pos)
+                pad.SetPosition(pos)
+                pad.SetLayerSet(pad.ConnSMDMask())
+                pad.GetParent().Add(pad)
+                angle = angle + step_angle
+                
+            if angle != last_angle:
+                angle = last_angle                
+                posX = math.cos (math.radians(angle)) * radius
+                posY = math.sin (math.radians(angle)) * radius
+                pad = PA.PadMaker(self.module).SMDPad(p_trace_width, p_trace_width, shape=pcbnew.PAD_SHAPE_RECT, rot_degree=-angle)
+                pos = self.draw.TransformPoint(posX, posY)
+                pad.SetPadName(1+j)
+                pad.SetPos0(pos)
+                pad.SetPosition(pos)
+                pad.SetLayerSet(pad.ConnSMDMask())
+                pad.GetParent().Add(pad)
+        
+        # circle on silkscreen
+        body_radius = (p_diameter/2 + p_trace_width/2 + self.draw.GetLineTickness())
+        self.draw.Circle(0, 0, body_radius)
+    
+    def BuildThisFootprint(self):
+
+        prm = self.parameters['Pads']
+        p_diameter = prm['diameter'];
+        p_style = prm['*style']
+         
+        if p_style == 1:
+            self.square_contact()
+        else:
+            self.round_contact() 
+
+        text_size = self.GetTextSize()  # IPC nominal
+        thickness = self.GetTextThickness()
+        body_radius = (p_diameter/2 + self.draw.GetLineTickness())
+        textposy = body_radius + self.draw.GetLineTickness()/2 + self.GetTextSize()/2 + thickness
+        self.draw.Value( 0, textposy, text_size )
+        self.draw.Reference( 0, -textposy, text_size )
+
+
+
+contact_wizard().register()
---
+++ scripts/footprint-wizards/dial_indicator_wizard.py
@@ -0,0 +1,169 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+# Authors: Bob Cousins
+# Created : 2018-01-01
+
+from __future__ import division
+
+import sys
+import math
+
+import pcbnew
+import HelpfulFootprintWizardPlugin as HFPW
+import PadArray as PA
+
+def rotate_about(center, p, theta):
+    dx = p.x-center.x
+    dy = p.y-center.y
+    theta = math.radians(theta)
+    x2 = dx * math.cos(theta) + dy * math.sin(theta)
+    y2 = dx * math.sin(theta) + dy * math.cos(theta)
+
+    return pcbnew.wxPoint(center.x+x2, center.y+y2)
+
+class dial_indicator_wizard(HFPW.HelpfulFootprintWizardPlugin):
+
+    def GetName(self):
+        return "Dial indicator"
+
+    def GetDescription(self):
+        return "Indicator for rotary control"
+
+    def GenerateParameterList(self):
+
+        self.AddParam("Dial", "angle", self.uNatural, 270)
+        self.AddParam("Dial", "offset angle", self.uNatural, 0)
+        self.AddParam("Dial", "radius", self.uMM, 6)
+        self.AddParam("Dial", "inner arc", self.uBool, 1)
+        self.AddParam("Dial", "outer arc", self.uBool, 0)
+
+        self.AddParam("Ticks", "show ticks", self.uBool, 1)
+        self.AddParam("Ticks", "number of divisions", self.uNatural, 10)
+        self.AddParam("Ticks", "tick length", self.uMM, 1)
+
+        self.AddParam("Labels", "show labels", self.uBool, 1)
+        self.AddParam("Labels", "min", self.uNatural, 0)
+        self.AddParam("Labels", "step", self.uNatural, 1)
+        
+
+    def CheckParameters(self):
+
+        self.CheckParamInt("Dial", "*angle", min_value=1, max_value=360)
+        self.CheckParamInt("Dial", "*offset angle", min_value=-180, max_value=180)
+        self.CheckParamBool("Dial", "*inner arc")
+        self.CheckParamBool("Dial", "*outer arc")
+
+        self.CheckParamBool("Ticks", "*show ticks")
+        self.CheckParamInt("Ticks", "*number of divisions", min_value=1)
+
+        self.CheckParamBool("Labels", "*show labels")
+
+
+    def GetValue(self):
+        
+        return "indicator"
+
+    
+    def BuildThisFootprint(self):
+
+        dial_params = self.parameters['Dial']
+        dial_angle = dial_params['*angle']
+        dial_offset_angle = dial_params['*offset angle']
+        dial_radius = dial_params['radius']
+        dial_inner_arc = dial_params['*inner arc']
+        dial_outer_arc = dial_params['*outer arc']
+        
+        tick_params = self.parameters ['Ticks']
+        tick_show = tick_params['*show ticks']
+        tick_num_divisions = tick_params['*number of divisions']
+        tick_length = tick_params['tick length']
+
+        label_params = self.parameters ['Labels']
+        label_show = label_params['*show labels']
+        label_min = label_params['*min']
+        label_step = label_params['*step']
+
+        text_size = self.GetTextSize()  # IPC nominal
+        thickness = self.GetTextThickness()
+        # self.draw.GetLineTickness())
+        textposy = self.GetTextSize()
+         
+        self.draw.Value( 0, textposy, text_size )
+        self.draw.Reference( 0, -textposy, text_size )
+
+        layer = pcbnew.F_SilkS
+        center = pcbnew.wxPointMM(0,0)
+
+        # inner arc
+        if dial_inner_arc:
+            start = pcbnew.wxPoint (center.x, center.y - dial_radius)
+            start = rotate_about (center, start, dial_angle/2.0+ dial_offset_angle)
+
+            self.draw.SetLayer(layer)
+            self.draw.SetLineTickness(pcbnew.FromMM (0.15))
+            self.draw.Arc (center.x, center.y, start.x, start.y, dial_angle*10)
+
+        # outer arc
+        if dial_outer_arc:
+            start = pcbnew.wxPoint (center.x, center.y - dial_radius - tick_length)
+            start = rotate_about (center, start, dial_angle/2.0+ dial_offset_angle)
+            self.draw.SetLayer(layer)
+            self.draw.SetLineTickness(pcbnew.FromMM (0.15))
+            self.draw.Arc (center.x, center.y, start.x, start.y, dial_angle*10)
+
+        # center cross
+        self.draw.SetLayer(layer)
+        self.draw.SetLineTickness(pcbnew.FromMM (0.15))
+        start = pcbnew.wxPointMM(-2, 0)
+        end = pcbnew.wxPointMM(2, 0)
+        self.draw.Line (start.x, start.y, end.x, end.y)
+        start = pcbnew.wxPointMM(0, -2)
+        end = pcbnew.wxPointMM(0, 2)
+        self.draw.Line (start.x, start.y, end.x, end.y)
+
+        for j in range (0, tick_num_divisions+1):
+            ang = j * dial_angle/tick_num_divisions - dial_angle/2.0 - dial_offset_angle
+            ang = -ang
+
+            start = pcbnew.wxPoint (center.x, center.y - dial_radius)
+            end = pcbnew.wxPoint (center.x, center.y - dial_radius - tick_length)
+
+            start = rotate_about (center, start, ang)
+            end = rotate_about (center, end, ang)
+
+            #
+            if tick_show:
+                self.draw.SetLayer(layer)
+                self.draw.SetLineTickness(pcbnew.FromMM (0.15))
+                self.draw.Line (start.x, start.y, end.x, end.y)
+
+            if label_show and j * dial_angle/tick_num_divisions < 360:
+                text_pos = pcbnew.wxPoint (center.x, center.y - dial_radius - tick_length - text_size*1.25)
+                text_pos = rotate_about (center, text_pos, ang)
+
+                #
+                text = pcbnew.TEXTE_MODULE(self.module)
+
+                text.SetPosition (text_pos)
+                #text.SetOrientation (ang*10.0)
+                text.SetThickness (pcbnew.FromMM (0.15))
+                text.SetLayer(layer)
+                text.SetText (str(label_min + j*label_step))
+
+                self.module.Add (text)
+
+
+dial_indicator_wizard().register()
